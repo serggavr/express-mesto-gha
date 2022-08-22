80d81e08a2504faf1504ed149e106287821d5d16
GSergey <g.sergey@bk.ru>
2022-08-22 23:01:35 +0300

    fix: errorHandler { error: err.message } to { message: err.message }

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,4 +1,4 @@
 module.exports.errorHandler = (err, req, res, next) => {
-  res.status(err.statusCode).json({ error: err.message });
+  res.status(err.statusCode).json({ message: err.message });
   next();
 };
